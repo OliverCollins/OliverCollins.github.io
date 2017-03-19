a00ccaf900d6f67fcf02d6a365b5e61f00611bfb
Oliver Collins <Oliver.Collins@Colorado.edu>
2017-03-19 11:05:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-$(function() {
-  "use strict";
 
-  $('html').removeClass('no-js').addClass('js');
-  
-  $('#typed').typed({
-    stringsElement: $('#typed-strings'),
-    loop: true,
-    backDelay: 2000
-  })
-
-});
